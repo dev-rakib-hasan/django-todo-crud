585d0846d69f7c0a091412337cc83c15c1a5ef10
dev-rakib-hasan <mrakibh67@gmail.com>
2026-08-10 22:21:35 +0600

    add todo serializer

--- Accounts/models.py
+++ Accounts/models.py
@@ -3,8 +3,10 @@ from django.db import models
 # Create your models here.
 class Todo (models.Model):
     title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
     created = models.BooleanField(default=False)
-    created_add = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
