0831cd91c5966c7556c493bc34f5eee309c3b9f4
dev-rakib-hasan <mrakibh67@gmail.com>
2026-08-13 11:13:24 +0600

    add update api

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
   path('todos/create/', views.create, name='todo-create'),
+  path('todos/<int:pk>/update', views.update, name = 'todo-update'),
 ]
--- Accounts/views.py
+++ Accounts/views.py
@@ -14,5 +14,23 @@ def create(request):
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
+@api_view(["GET", "PUT"])
+def update(request, pk):
+    try:
+        todo = Todo.objects.get(pk=pk)
+    except Todo.DoesNotExist:
+        return Response({'message': 'Todo Not Found'}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == "GET":
+        serializer = Todoserializer(todo)
+        return Response({'message': 'Todo Fetched Successfully', 'data': serializer.data}, status=status.HTTP_200_OK)
+
+    
+    serializer = Todoserializer(todo, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({'message': 'Todo Update Successfully', 'data':serializer.data}, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
+
 
 
